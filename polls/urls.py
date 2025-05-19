67549a23c3ebd4c07f9c91eaa0a068d6c0af8a49
ZiluWang <roww3061@Gmail.com>
2025-05-19 23:24:43 +0800

    part 3

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,24 @@ from django.urls import path
 
 from . import views
 
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
+
+
+# Leave the rest of the views (detail, results, vote) unchanged
